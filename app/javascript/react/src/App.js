627b443586fa9e99fe12148b42b313529f1120d0
joemusacchia <jmusacchia@joesmac-2.fios-router.home>
2017-11-17 01:19:45 -0500

    initial config with webpacker - react - JS test suite

---
+++ app/javascript/react/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Router, browserHistory, Route, IndexRoute } from 'react-router';
+
+const App = props => {
+  return(
+    <h1>Hello World</h1>
+  )
+}
+
+export default App
